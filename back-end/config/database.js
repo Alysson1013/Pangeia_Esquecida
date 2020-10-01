a5882be026db2133c620337f1fc6bc5d78c57bd7
Alysson1013 <alysson6801@hotmail.com>
2020-10-01 11:51:56 +0000

    (01/09) Commit após pull.

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
 //conexão do mongo
-=======
-//Conexão com banco de dados
->>>>>>> 39a2d6b29d0210d47f49c0fb7ab2413101fc52fd
 module.exports = uri => {
     mongoose.connect(uri, {
         useNewUrlParser: true,
@@ -22,13 +18,7 @@ module.exports = uri => {
         process.exit(0)
     })
 
-<<<<<<< HEAD
     mongoose.connection.on('disconnected', () => {
         console.log('Moongose desconectado')
-=======
-    //Dá sinal de queda 
-    mongoose.connection.on('disconnected', ()=>{
-        console.log('==> Mongoose! desconectado do servidor')
->>>>>>> 39a2d6b29d0210d47f49c0fb7ab2413101fc52fd
     })
 }
