1df6caa5eb7e48bdcedc3a43788b8865ff039582
Alysson1013 <alysson6801@hotmail.com>
2020-12-08 14:59:44 +0000

    (08/12) Finalização do Projeto 😎

--- front-end/src/app/paleontologo/paleontologo-list/paleontologo-list.component.ts
+++ front-end/src/app/paleontologo/paleontologo-list/paleontologo-list.component.ts
@@ -1,3 +1,4 @@
+import { CriaturaService } from './../../criatura/criatura.service';
 import { Component, OnInit } from '@angular/core';
 import { PaleontologoService } from '../paleontologo.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,17 +13,21 @@ export class PaleontologoListComponent implements OnInit {
   searchText
 
   paleontologos : any = []  // Vetor vazio
+  criaturas : any = []
 
   displayedColumns : string[] = ['nome', 'data_nascimento', 'data_falecimento', 'descr', 'especializacao', 'imagem', 'editar', 'excluir']
   
   constructor(
+    private criaturaSrv : CriaturaService,
     private paleontologoSrv : PaleontologoService,
     private snackBar : MatSnackBar
   ) { }
 
   async ngOnInit() {
     this.paleontologos = await this.paleontologoSrv.listar()
+    this.criaturas = await this.criaturaSrv.listar()
     console.log(this.paleontologos)
+    console.log(this.criaturas)
   }
 
   async excluir(id : string) {
